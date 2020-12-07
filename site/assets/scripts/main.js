933bdc06e73e46194831fd8bfd55524c1166ae8b
muji <muji@tmpfs.org>
2020-12-07 09:33:03 +0800

    Include lock file.

--- site/assets/scripts/main.js
+++ site/assets/scripts/main.js
@@ -1,7 +1,6 @@
 const run = () => {
   const search = document.querySelector(".search-wrapper");
   const search_toggle = document.querySelector('[href="#search"]');
-
   search_toggle.addEventListener('click', (e) => {
     e.preventDefault();
     search.classList.toggle('hidden');
--- site/index.md
+++ site/index.md
@@ -6,4 +6,6 @@ Posts about the [UWE project][uwe]; insights and news on progress.
 
 ### Recent Posts
 
+{{log (json this pretty=true)}}
+
 {{>list}}
